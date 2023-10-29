209b61e680780435aea3b3d47f9a5b6d40bfe063
KevinJSalazar <137641381+KevinJSalazar@users.noreply.github.com>
2023-10-29 15:37:08 -0500

    Add files via upload

---
+++ Tarea01/src/main/java/ec/edu/espol/tarea01/Tarea01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.tarea01;
+
+/**
+ *
+ * @author evin
+ */
+public class Tarea01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
